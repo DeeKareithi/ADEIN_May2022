ab27825362b528abd9f67db49392fc0b0593b6ba
Dorcas Kareithi <19709060+DeeKareithi@users.noreply.github.com>
2022-04-28 10:59:22 +0100

    Update ADEIN_May2022_01_Reading_from_data_online_platform.r

--- ADEIN_May2022_01_Reading_from_data_online_platform.r
+++ ADEIN_May2022_01_Reading_from_data_online_platform.r
@@ -4,3 +4,11 @@
 
 #Theme: Use of Open Health data for decsion making.
 
+# load libraries: If these are not there, do install them first
+library(stringr)
+library(stringi)
+library(dplyr)
+library(readr)
+
+data <- read_csv("https://www.who.int/data/gho/data/indicators/indicator-details/GHO/antenatal-care-coverage-at-least-four-visits")
+
